9ac07934e5571a15321467096bbfa331255bc5ee
gwhite <grady.white@maine.edu>
2023-04-08 17:09:50 -0400

    Fixed first time use issue

--- config.c
+++ config.c
@@ -105,6 +105,9 @@ int verify_files(void)
             perror(strerror(errno));
             return -1;
         }
+        fprintf(config_file, "user: Default");
+        fprintf(config_file, "ncolor: 0");
+        fprintf(config_file, "pcolor: 0");
         fclose(config_file);
     } else {
         fclose(config_file);
